73745700d1ed76acb4807ebf2fc7e4f25fdb7558
Kerwin <1079091667@qq.com>
2017-05-07 15:34:59 +0800

    Windows轻量级音乐播放器
    IrisSkin4_VS2015，Interop.WMPLib，MFC换肤

---
+++ Music.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace QianQianMisc
+{
+    partial class Music
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Music));
+            this.skinEngine1 = new Sunisoft.IrisSkin.SkinEngine(((System.ComponentModel.Component)(this)));
+            this.btnChange = new System.Windows.Forms.Button();
+            this.musicPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnPlayOrPause = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.labInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // skinEngine1
+            // 
+            this.skinEngine1.SerialNumber = "";
+            this.skinEngine1.SkinFile = null;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(265, 12);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "换肤";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // musicPlayer
+            // 
+            this.musicPlayer.Enabled = true;
+            this.musicPlayer.Location = new System.Drawing.Point(12, 12);
+            this.musicPlayer.Name = "musicPlayer";
+            this.musicPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("musicPlayer.OcxState")));
+            this.musicPlayer.Size = new System.Drawing.Size(232, 288);
+            this.musicPlayer.TabIndex = 1;
+            this.musicPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.musicPlayer_PlayStateChange);
+            this.musicPlayer.Enter += new System.EventHandler(this.musicPlayer_Enter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 245);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnPlayOrPause
+            // 
+            this.btnPlayOrPause.Location = new System.Drawing.Point(265, 53);
+            this.btnPlayOrPause.Name = "btnPlayOrPause";
+            this.btnPlayOrPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayOrPause.TabIndex = 3;
+            this.btnPlayOrPause.Text = "播放";
+            this.btnPlayOrPause.UseVisualStyleBackColor = true;
+            this.btnPlayOrPause.Click += new System.EventHandler(this.btnPlayOrPause_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(265, 96);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "停止";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(346, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(152, 244);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "导入音乐";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "上一曲";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "下一曲";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // labInfo
+            // 
+            this.labInfo.AutoSize = true;
+            this.labInfo.Location = new System.Drawing.Point(265, 254);
+            this.labInfo.Name = "labInfo";
+            this.labInfo.Size = new System.Drawing.Size(41, 12);
+            this.labInfo.TabIndex = 10;
+            this.labInfo.Text = "label1";
+          
+
+            // 
+            // Music
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 312);
+            this.Controls.Add(this.labInfo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlayOrPause);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.musicPlayer);
+            this.Controls.Add(this.btnChange);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(496, 351);
+            this.Name = "Music";
+            this.Text = "音乐悦";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Sunisoft.IrisSkin.SkinEngine skinEngine1;
+        private System.Windows.Forms.Button btnChange;
+        private AxWMPLib.AxWindowsMediaPlayer musicPlayer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnPlayOrPause;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label labInfo;
+    }
+}
+
---
+++ Music.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//视频教程URL:http://dvd.boxuegu.com/course/125.html
+//Author:Kerwin(ckjbug)
+
+namespace QianQianMisc
+{
+    public partial class Music : Form
+    {
+        public Music()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //在程序加载的时候，给窗体程序更换一个皮肤，通过向船体中拖入skinEngine控件（数据链接文件）
+            skinEngine1.SkinFile = @"C:\Users\ckjbug\Documents\Visual Studio 2015\IrisSkin4_VS2015\IrisSkin4_VS2015\Skins\SteelBlack.ssk";
+            //在当前的pictureBox赋值一张默认的图片,可以是GIF图片
+            pictureBox1.Image = Image.FromFile(@"C:\Users\ckjbug\Pictures\Camera Roll\软件小图标\qq表情\3887\11.gif");
+            //设置图片填充pictureBox1的大小布局   
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            //为了避免播放器一打开就播放音乐,取消自动播放
+            musicPlayer.settings.autoStart = false;
+            //musicPlayer.URL = @"F:\手机资料\手机音乐\Beautiful Times-Owl City.mp3";
+            //接下来，我们将Media player上的播放等功能按钮自己来实现
+        }
+
+
+        int i = 0;
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            //把所有的皮肤都读取进来，取每个皮肤的全路径（其实就是一个个字符串），存入到一个字符串数组stylePath中
+            string[] stylePath = Directory.GetFiles(@"C:\Users\ckjbug\Documents\Visual Studio 2015\IrisSkin4_VS2015\IrisSkin4_VS2015\Skins");
+            //点击换肤其实就是在stylePath数组中拿去一个皮肤的全路径字符串数值给SkinFile
+            i++;//让皮肤发生变化
+            if (i == stylePath.Length)
+            {
+                i = 0;//当所有皮肤换完之后又从第一个皮肤开始换肤
+            }
+            skinEngine1.SkinFile = stylePath[i];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timer组件为pictureBox（覆盖在Media player视频播放上面的黑色区域）定时更换一张图片
+            //和上面更换文件的事件一样，首先要读取文件（这里是图片文件）
+            string[] imgPath = Directory.GetFiles(@"C:\Users\ckjbug\Pictures\Camera Roll\软件小图标\qq表情\3887");
+            i++;
+            if (i == imgPath.Length) {
+                i = 0;
+            }
+                pictureBox1.Image = Image.FromFile(imgPath[i]);//从文件中提取图片路径字符串为pictureBox的Image属性赋值
+           
+        }
+
+        //实现播放器的播放或暂停
+        private void btnPlayOrPause_Click(object sender, EventArgs e)
+        {
+            if (btnPlayOrPause.Text == "播放")
+            {
+                musicPlayer.Ctlcontrols.play();
+                //接下来将按钮的文本改为暂停
+                btnPlayOrPause.Text = "暂停";
+
+            }
+            else if (btnPlayOrPause.Text == "暂停") {
+                musicPlayer.Ctlcontrols.pause();
+                btnPlayOrPause.Text = "播放";
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            //点击停止就只需调用stop函数即可
+            musicPlayer.Ctlcontrols.stop();
+        }
+        
+        //用来存储音乐文件的全路径
+        List<string> listSongs = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //导入选择的音乐到ListBox播放队列中
+            OpenFileDialog ofd = new OpenFileDialog();
+            //在Show出来之前，设置一下对话框的属性，对话框
+            ofd.Title = "请选择要播放的音乐文件！";
+            //设置对话框可多选
+            ofd.Multiselect = true;
+            //设置打开文件类型
+            ofd.Filter = "音乐文件|*.mp3|所有文件|*.*";
+            //设置打开文件的初始默认路径
+            ofd.InitialDirectory = @"F:\手机资料\手机音乐";
+            //展示对话框
+            ofd.ShowDialog();
+            //多选，获得我们在对话框中选中文件的全路径
+            string[] filePath = ofd.FileNames;
+            //根据全路径截取文件名加载到ListBox中
+            //需要将数组中放入全路径存储起来
+            for (int i = 0; i < filePath.Length; i++) {
+                //将全路径存储到集合List中
+                listSongs.Add(filePath[i]);
+                //将音乐文件名取出来防止ListBox列表中显示出来
+                listBox1.Items.Add(Path.GetFileName(filePath[i]));
+            }
+        }
+
+        //双击播放
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1) {
+                return;
+            }
+            //当我们双击一首音乐的时候，我们需要双击这个文件名所对应的全路径
+
+            musicPlayer.URL=listSongs[listBox1.SelectedIndex];//当前所选的文件索引
+            musicPlayer.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            //下一曲
+            //获得当前选中项的索引
+            int index = listBox1.SelectedIndex;
+            //因为我们在listBox1中添加了多选属性
+            //将之前选中项的索引都清空，这样能够确保我=我们之后的歌曲被单独选中
+            listBox1.SelectedIndices.Clear();
+            if (index == -1)
+            {
+                return;
+            }
+            index++;
+            //我在第一首歌点击了上一曲,应该回到最后一首歌
+            if (index == listBox1.Items.Count)
+            {
+                index = 0;
+            }
+            //将改变后的索引是赋值给当前选中项的索引,这样才会改变ListBox里的状态
+            listBox1.SelectedIndex = index;
+            //通过索引区泛型集合里面拿到的全路径 复制给播放器的url属性
+            musicPlayer.URL = listSongs[index];
+            musicPlayer.Ctlcontrols.play();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //上一曲
+            //获得当前选中项的索引
+            int index = listBox1.SelectedIndex;
+            //将之前选中项的索引都清空，这样能够确保我=我们之后的歌曲被单独选中
+            listBox1.SelectedIndices.Clear();
+            index--;
+            //我在第一首歌点击了上一曲,应该回到最后一首歌
+            if (index < 0) {
+                index = listBox1.Items.Count - 1;
+            }
+            //将改变后的索引是赋值给当前选中项的索引,这样才会改变ListBox里的状态
+            listBox1.SelectedIndex = index;
+            //通过索引区泛型集合里面拿到的全路径 复制给播放器的url属性
+            musicPlayer.URL = listSongs[index];
+            musicPlayer.Ctlcontrols.play();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        //多选删除音乐  导入右键菜单跟ListBox1列表关联
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //先获得要清楚歌曲的数量,通过循环一条条删除
+            int count = listBox1.SelectedItems.Count;
+            //删除两个地方1，Listbox1这个列表；2，listSongs这个集合   先删谁，怎么删？？先删集合再删索引,
+            //这样就保证了要删除数据的索引一直都在，如果先删除列表，那么索引也随着消失了，那么集合就拿不到索引了
+            for (int i = 0; i < count; i++) {
+                listSongs.RemoveAt(listBox1.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //前提是当前正在播放音乐，不让是不会显示下面的信息
+            if (musicPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                //显示歌曲信息
+                labInfo.Text = musicPlayer.Ctlcontrols.currentPosition + "\r\n" + musicPlayer.Ctlcontrols.currentPositionString + "\r\n" + musicPlayer.currentMedia.duration + "\r\n" + musicPlayer.currentMedia.durationString;
+
+                //1，自动播放下一曲----通过从歌曲信息的时间进行下一曲（bug）
+               /*
+                //如果当前播放的总时间减去正在播放的总时间小于等于1的时候就可以进行下一曲了
+                if (musicPlayer.currentMedia.duration - musicPlayer.Ctlcontrols.currentPosition <= 1) {
+                    //将下一曲中的代码全部拷贝进来即可
+                    //下一曲
+                    //获得当前选中项的索引
+                    int index = listBox1.SelectedIndex;
+                    //因为我们在listBox1中添加了多选属性
+                    //将之前选中项的索引都清空，这样能够确保我=我们之后的歌曲被单独选中
+                    listBox1.SelectedIndices.Clear();
+                    if (index == -1)
+                    {
+                        return;
+                    }
+                    index++;
+                    //我在第一首歌点击了上一曲,应该回到最后一首歌
+                    if (index == listBox1.Items.Count)
+                    {
+                        index = 0;
+                    }
+                    //将改变后的索引是赋值给当前选中项的索引,这样才会改变ListBox里的状态
+                    listBox1.SelectedIndex = index;
+                    //通过索引区泛型集合里面拿到的全路径 复制给播放器的url属性
+                    musicPlayer.URL = listSongs[index];
+                    musicPlayer.Ctlcontrols.play();
+
+                }
+               
+                 */
+
+            }
+
+
+        }
+
+        //2，自动播放下一曲----根据播放器的状态自动进行下一曲就是一首歌结束完之后再自动播放下一曲--Ready,Playing,Pause,Ended,Sotp四种状态
+        //当播放起的状态到达Ended的时候，进行下一曲
+        //在Ready之后我们进行播放Play()---先在Media Player 控件中设置
+
+
+        private void musicPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            //当播放器的播放状态发生改变的时候，判断当前的的音乐播放器的状态是否达到Enden，如果是Ended，则进行下一曲
+            if (musicPlayer.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+            {
+                //下一曲(拷贝下一曲中的代码)
+
+                //获得当前选中项的索引
+                int index = listBox1.SelectedIndex;
+                //因为我们在listBox1中添加了多选属性
+                //将之前选中项的索引都清空，这样能够确保我=我们之后的歌曲被单独选中
+                listBox1.SelectedIndices.Clear();
+                if (index == -1)
+                {
+                    return;
+                }
+                index++;
+                //我在第一首歌点击了上一曲,应该回到最后一首歌
+                if (index == listBox1.Items.Count)
+                {
+                    index = 0;
+                }
+                //将改变后的索引是赋值给当前选中项的索引,这样才会改变ListBox里的状态
+                listBox1.SelectedIndex = index;
+                //通过索引区泛型集合里面拿到的全路径 复制给播放器的url属性
+                musicPlayer.URL = listSongs[index];
+            }
+
+            //在Ready之后才能开始播放
+            if (musicPlayer.playState == WMPLib.WMPPlayState.wmppsReady)
+            {
+                //捕获异常
+                try
+                {
+                    musicPlayer.Ctlcontrols.play();
+                }
+                catch { }
+            }
+
+
+        }
+
+
+
+
+        //添加歌词模块,用一个Lable控件实现.No2:40minutes.待实现...
+
+       
+
+        private void musicPlayer_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+    }
+}
